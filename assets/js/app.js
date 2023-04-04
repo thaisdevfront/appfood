4c08da18256abd8cddaf0bad09d549cbd2b7e808
thaisdevfront <thais.devfrontend@gmail.com>
2023-04-04 12:25:23 -0300

    up

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+cardapiobd=[]
+
+var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+  var swiper = new Swiper(".mySwiperTabs", {
+          slidesPerView: 3,
+          cssMode: true,
+           autoplay: {
+          delay: 5500,
+          disableOnInteraction: false,
+          },
+          
+          });
+
+ 
+
+
